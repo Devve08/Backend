8e48e59c8a102d815e8c59ab968748f1cf75a8e0
Antoine Debes <antoinedebes@gmail.com>
2021-07-02 10:26:12 +0300

    Created Antoine-branch branch

--- server.js
+++ server.js
@@ -1,40 +1,42 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv/config')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
-const productRouter = require('./routes/product')
-const userRouter = require('./routes/user')
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
 
-
-const app = express()
+const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // connecting to database
 
-mongoose.connect(
-    process.env.ATLAS_URI, 
-    {useNewUrlParser : true,
-     useUnifiedTopology: true})
+mongoose.connect(process.env.ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connection.once("open", () => {
-    console.log("connected")
-})
+mongoose.connection
+  .once("open", () => {
+    console.log("connected");
+  })
+  .on("e", (e) => {
+    console.log(e);
+  });
 
-app.use('/product', productRouter)
-app.use('/user', userRouter)
+app.use("/product", productRouter);
+app
+  .use("/user", userRouter)
 
-.on("error", (error) => {
-    console.log("your error", error)
-})
-
-
+  .on("error", (error) => {
+    console.log("your error", error);
+  });
 
 //listening to port
 
-app.listen(port, ()=> {
-    console.log(`Server is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
