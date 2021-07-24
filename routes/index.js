f09d328289231c752db6dbc22e108a878169d013
Antoine Debes <Antoinedebes@gmail.com>
2021-07-24 15:31:38 +0300

    Removed some stuffs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import productRouter from "./product.js";
 import userRouter from "./user.js";
-import cartRouter from "./cart.js";
 
 const appRoutes = Router();
 
