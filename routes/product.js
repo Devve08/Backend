23de5af3072af7b8edcf8015f585a9500d3b4a0d
Devve08 <mhamad.safa.08@gmail.com>
2021-08-05 12:07:22 +0300

    changes

--- routes/product.js
+++ routes/product.js
@@ -1,26 +1,16 @@
 import express from "express";
 import { getProducts, addProducts } from "../controllers/product.js";
 import { checkUser } from "../middleware/authMiddleware.js";
-import multer from "multer";
+
 
 const productRouter = express.Router();
 
-let storage = multer.diskStorage({
-    destination:function(req, file, cb){
-        cb(null, 'uploads')
-    },
-    filename: function(req, file, cb){
-        cb(null, file.originalname)
-    }
-})
-
-const upload = multer({storage : storage})
 
 
 productRouter.get("*", checkUser);
 
 productRouter.get("/", getProducts);
 
-productRouter.post("/create", upload.single("productImage") ,addProducts);
+productRouter.post("/create" ,addProducts);
 
 export default productRouter;
