2fe6ec6c56e09f417784ca9afaaacb65d83af5df
Devve08 <mhamad.safa.08@gmail.com>
2021-08-05 14:51:58 +0300

    make it better

--- server.js
+++ server.js
@@ -42,9 +42,9 @@ app.listen(port, () => {
 });
 
 if (process.env.NODE_ENV == 'production'){
-  app.use(express.static(path.resolve(__dirname, '../Backend/build')));
+  app.use(express.static(path.resolve(__dirname, '../frontend/frontend/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../Backend/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../frontend/frontend/build', 'index.html'));
 });
